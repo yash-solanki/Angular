2cfa2665bf7aeb514e7559ddc877219532959c81
yashsolanki <yashsolanki153@gmail.com>
2019-03-01 19:03:09 +0530

    Angular work

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'IronMan' },
+    { id: 12, name: 'Caption America' },
+    { id: 13, name: 'Thor' },
+    { id: 14, name: 'Hulk' },
+    { id: 15, name: 'Vision' },
+    { id: 16, name: 'SpiderMan' },
+    { id: 17, name: 'Black Widow' },
+    { id: 18, name: 'Black Panther' },
+    { id: 19, name: 'Quick Silver' },
+    { id: 20, name: 'Hawkeye' }
+]
