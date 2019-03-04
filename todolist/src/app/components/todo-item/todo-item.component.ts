a09d19e19f19567e7a57d0f5a6b8a27107c93c1b
yashsolanki <yashsolanki153@gmail.com>
2019-03-04 18:06:48 +0530

    Todo list insert update delete by angular

---
+++ todolist/src/app/components/todo-item/todo-item.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Todo } from 'src/app/models/Todo';
+import { TodoService } from '../../services/todo.service';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css']
+})
+export class TodoItemComponent implements OnInit {
+
+  @Input() todo: Todo ;
+  @Output() deleteTodo: EventEmitter<Todo> = new EventEmitter();
+
+  constructor(private todoService: TodoService) { }
+
+  ngOnInit() {
+  }
+  //Set Dynamic Classes
+  setClasses() {
+    let Classes = {
+      todo: true,
+      'is-complete': this.todo.completed
+    }
+    return Classes;
+  }
+  onToggle(todo) {
+
+    //toggle on ui
+    todo.completed = !todo.completed;
+    //toggle on server
+    this.todoService.toggleCompleted(todo).subscribe(todo => {
+      console.log(todo);
+    })
+  }
+  onDelete(todo) {
+    this.deleteTodo.emit(todo);
+  }
+
+}
