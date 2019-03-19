948a8cbb8f663fb3cf657b6d0abec21e58aa0faf
yashsolanki <yashsolanki153@gmail.com>
2019-03-19 18:07:13 +0530

    changes

--- LoginApp/backend/server.js
+++ LoginApp/backend/server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const bodyparser = require('body-parser');
 const app= express();
 
+
 mongoose.connect("mongodb://localhost:27017/People", { useNewUrlParser: true });
 
 let People = mongoose.model("People", {
