97820b20ac040daa3c1520f8eb458edc83d56903
yash-solanki <yashsolanki153@gmail.com>
2019-03-24 22:41:31 +0530

    Local storage removed

--- LoginCRUD/frontend/src/app/register/register.component.ts
+++ LoginCRUD/frontend/src/app/register/register.component.ts
@@ -27,7 +27,7 @@ export class RegisterComponent implements OnInit {
       .subscribe(
         res => {
           console.log(res);
-          localStorage.setItem('token', res.token);
+          // localStorage.setItem('token', res.token);
           this.router.navigate(['/login']);
         },
         err => console.log(err)
