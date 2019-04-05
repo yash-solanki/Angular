3c30e14563078fe242d1cf71d7cf4fdf603b686a
yashsolanki <yashsolanki153@gmail.com>
2019-04-05 17:27:43 +0530

    Added path of fetchToken

--- LoginCRUD/frontend/src/app/app-routing.module.ts
+++ LoginCRUD/frontend/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { AuthGuard } from './auth.guard';
 import { CreateComponent } from './component/create/create.component';
 import { EditComponent } from './component/edit/edit.component';
 import { ListComponent } from './component/list/list.component';
+import { FetchTokenComponent } from './fetch-token/fetch-token.component';
 
 const routes: Routes = [
   {
@@ -22,6 +23,7 @@ const routes: Routes = [
   { path: 'create', component: CreateComponent, canActivate: [AuthGuard] },
   { path: 'edit/:id', component: EditComponent, canActivate: [AuthGuard] },
   { path: 'list', component: ListComponent, canActivate: [AuthGuard] },
+  { path: 'ftoken', component: FetchTokenComponent }
 ];
 
 @NgModule({
