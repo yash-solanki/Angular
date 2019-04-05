3e0b6365353c4e85bd0030381104592f45009b4d
yashsolanki <yashsolanki153@gmail.com>
2019-04-05 17:26:00 +0530

    Added path for passToken

--- LoginCRUD/backend/routes/api.js
+++ LoginCRUD/backend/routes/api.js
@@ -4,6 +4,8 @@ const User = require('../models/user');
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const controller = require('../controller/controller');
+const socialctrl = require('../controller/googlectrl');
+const facectrl = require('../controller/facebookctrl');
 
 mongoose.connect('mongodb://localhost:27017/Ausers', { useNewUrlParser: true }, err => {
     if (err) {
@@ -21,6 +23,18 @@ mongoose.connect('mongodb://127.0.0.1/issues',{ useNewUrlParser: true }, err => 
     }
 });
 
+// Social Path
+
+router.get('/checkauth', socialctrl.checkauth);
+
+router.post('/passToken', controller.verify, socialctrl.dealWithToken);
+
+router.get('/faceauth', facectrl.auth);
+
+router.get('/faceredirect', facectrl.callback);
+
+
+
 //Login
 router.post('/logout', controller.LogoutUser);
 
@@ -31,9 +45,9 @@ router.post('/register', controller.RegisterUser);
 router.post('/login', controller.LoginUser);
 
 //CRUD
-router.get('/', controller.verify, controller.getAllIssues);
+router.get('/',controller.verify, controller.getAllIssues);
 
-router.get('/:id', controller.verify ,controller.getIssueById);
+router.get('/:id', controller.verify, controller.getIssueById);
 
 router.post('/', controller.verify ,controller.addIssue);
 
