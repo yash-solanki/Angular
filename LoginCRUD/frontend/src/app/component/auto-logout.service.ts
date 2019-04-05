21260387ef176a153a353792fcd6034b926c96d7
yashsolanki <yashsolanki153@gmail.com>
2019-04-05 17:31:52 +0530

    Added autologout feature

--- LoginCRUD/frontend/src/app/component/auto-logout.service.ts
+++ LoginCRUD/frontend/src/app/component/auto-logout.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AuthService } from '../auth.service';
 
-const MINUTES_UNTIL_AUTO_LOGOUT = 0.10; // in mins
+const MINUTES_UNTIL_AUTO_LOGOUT = 0.20; // in mins
 const CHECK_INTERVAL = 1000; // in ms
 const STORE_KEY = 'lastAction';
 
@@ -34,21 +34,22 @@ export class AutoLogoutService {
   }
   initInterval() {
     setInterval(() => {
-      if (this.authService.loggedIn()) {
-
-      this.check();
+      if (this.authService.loggedIn() || this.authService.loggedInGoogle() ) {
+        this.check();
       }
     }, CHECK_INTERVAL);
   }
 
   check() {
     const now = Date.now();
+    console.log('now' + ':' + now);
     const timeleft = this.lastAction + MINUTES_UNTIL_AUTO_LOGOUT * 60 * 1000;
+    console.log('timeleft' + ':' + timeleft);
     const diff = timeleft - now;
     console.log(diff);
     const isTimeout = diff < 0;
-    if (isTimeout && this.authService.loggedIn()) {
-      console.log(this.authService.loggedIn());
+    if (isTimeout && (this.authService.loggedIn() || this.authService.loggedInGoogle() )) {
+      // console.log(this.authService.loggedIn());
       this.authService.logoutUser();
     }
   }
