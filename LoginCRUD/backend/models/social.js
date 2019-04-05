3356c192d2dbdd17bddd2cabefb44a2de796ca68
yashsolanki <yashsolanki153@gmail.com>
2019-04-05 17:38:06 +0530

    Added schema for google user

---
+++ LoginCRUD/backend/models/social.js
@@ -0,0 +1,17 @@
+let mongo = require('mongoose');
+mongo.connect('mongodb://127.0.0.1/yash',{ useNewUrlParser: true }, err => {
+  if (err) {
+      console.error(err);
+  } else {
+      console.log('Connected to Database 1233');
+  }
+});
+let UserSchema=new mongo.Schema({
+    email:{ 
+      type:String
+    },
+    displayName: { 
+        type:String
+    }
+});
+module.exports = mongo.model('people', UserSchema, 'yash');
