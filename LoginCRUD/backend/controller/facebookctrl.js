5d894f75523da8509612f0aeed6a13120299c86a
yashsolanki <yashsolanki153@gmail.com>
2019-04-08 18:02:06 +0530

    Added code for facebook auth

--- LoginCRUD/backend/controller/facebookctrl.js
+++ LoginCRUD/backend/controller/facebookctrl.js
@@ -1,51 +1,55 @@
 const express = require('express');
 const app = express();
 const passport = require('passport');
-const FacebookStrategy = require('passport-facebook').Strategy;
+const FacebookStrategy = require('passport-facebook');
 const auth = require('../pass.json');
+const Jwt = require('jsonwebtoken');
 
-
+// passport.use(new FacebookStrategy({
+//     clientID: '861017947402225',
+//     clientSecret: 'e795fa45776390ca769b0291a73e64b0',
+//     callbackURL: 'http://localhost:4040/auth/facebook/redirect'
+// },
+//     function (accessToken, refreshToken, profile, done) {
+//         done(null, profile);
+//     }
+// ));
 
 passport.use(new FacebookStrategy({
     clientID: auth.web.clientID,
     clientSecret: auth.web.clientSecret,
     callbackURL: auth.web.callbackURL
-},
+    },
     function (accessToken, refreshToken, profile, done) {
+        // console.log(profile);
         done(null, profile);
+        
     }
 ));
 
-passport.serializeUser(function (user, done) {
-    done(null, user);
+passport.serializeUser((user, done) => {
+    done(null, user.id);
 });
 
-passport.deserializeUser(function (user, done) {
-    done(null, user);
+passport.deserializeUser((id, done) => {
+    mongodb.findById(id).then((user) => {
+        done(null, user);
+    });
 });
 
-exports.auth = passport.authenticate('facebook', {scope:["email"]});
+let fbtoken;
 
-// app.get('/auth/facebook',
-//     passport.authenticate('facebook', {scope:["email"]}));
+exports.faceauth = passport.authenticate('facebook',{ scope: ['user_friends', 'manage_pages'] });
 
-exports.callback = passport.authenticate('facebook', {
-    failureRedirect: '/fail',
-    successRedirect: 'http://localhost:4200/list'
-});
+exports.faceredirect = passport.authenticate('facebook', { failureRedirect: 'http://localhost:4200' }),
+                    function(req, res) {
+                        console.log(req.user);
+                        fbtoken=req.user.fbId
+                        res.redirect('/auth/loginfb');
+                    }
 
-// app.get('/auth/facebook/callback',
-//     passport.authenticate('facebook', {
-//         failureRedirect: '/fail',
-//         successRedirect: 'http://localhost:4200/list'
-// }));
-
-// app.get('/fail', (req, res) => {
-//     res.send('fail');
-// });
-
-
-
-// app.get('/', (req, res) => {
-//     res.redirect('/auth/facebook');
-// });
+exports.loginfb = (req, res) => {
+    const token=Jwt.sign({payload:fbtoken},'secret');
+    console.log(token);
+    res.redirect(`http://localhost:4200/login?token=${token}`);
+};    
