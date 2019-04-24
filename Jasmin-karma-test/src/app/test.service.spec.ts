8f11a97facfbe313bde360643bf02da012505535
yashsolanki <yashsolanki153@gmail.com>
2019-04-24 12:11:40 +0530

    Practice of how simple test case created and works

---
+++ Jasmin-karma-test/src/app/test.service.spec.ts
@@ -0,0 +1,23 @@
+import { TestBed } from '@angular/core/testing';
+
+import { TestService } from './test.service';
+
+describe('TestService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: TestService = TestBed.get(TestService);
+    expect(service).toBeTruthy();
+  });
+
+  it('should have add function', ()=> {
+    const service: TestService = TestBed.get(TestService);
+    expect(service.add).toBeTruthy();
+  });
+
+  it('should add correctly', () => {
+    const service: TestService = TestBed.get(TestService);
+    expect(service.add(1, 2)).toEqual(3);
+  });
+
+});
---
+++ Jasmin-karma-test/src/app/test.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TestService {
+
+  constructor() { }
+
+  add(a, b) {
+    return a + b;
+  }
+
+}
