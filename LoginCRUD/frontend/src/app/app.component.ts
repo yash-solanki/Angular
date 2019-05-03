f6f123c5811cad386804478d831d6ffc6824bf07
yashsolanki <yashsolanki153@gmail.com>
2019-05-03 12:45:27 +0530

    added method for checking loading or not

--- LoginCRUD/frontend/src/app/app.component.ts
+++ LoginCRUD/frontend/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { AuthService } from './auth.service';
 // import { AutoLogoutService } from './component/auto-logout';
 import { AutoLogoutService } from './component/auto-logout.service';
+import { Router, Event, NavigationStart, NavigationEnd, NavigationCancel, NavigationError } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -11,11 +12,26 @@ import { AutoLogoutService } from './component/auto-logout.service';
 export class AppComponent {
   title = 'frontend';
   token = false ;
+  loading = true;
 
-  constructor( public authService: AuthService, private autoLogout: AutoLogoutService ) {
+  constructor( public authService: AuthService, private autoLogout: AutoLogoutService, private router: Router ) {
     this.tokenFatch();
+    router.events.subscribe(( routerEvent: Event ) => {
+      this.checkRouterEvent(routerEvent);
+    });
   }
 
+  checkRouterEvent(routerEvent: Event): void {
+    if (routerEvent instanceof NavigationStart) {
+      this.loading = true;
+    }
+    if ( routerEvent instanceof NavigationEnd ||
+          routerEvent instanceof NavigationCancel ||
+          routerEvent instanceof NavigationError
+      ) {
+        this.loading = false;
+      }
+  }
 
   tokenFatch() {
     this.authService
